ef90bf96d6521bfb9f9db755db6060cf87a605ec
OGEmanuel <1blacksanta416@gmail.com>
2024-05-24 17:25:41 +0100

    Fixed main padding bottom

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Nav from "./nav";
 
 export default function Home() {
   return (
-    <main className="ml-40 border-b border-l !border-[#F3F4F6] bg-[url('/images/background.png')] bg-cover bg-fixed bg-center bg-no-repeat pb-6">
+    <main className="ml-40 border-b border-l !border-[#F3F4F6] bg-[url('/images/background.png')] bg-cover bg-fixed bg-center bg-no-repeat pb-16">
       <Nav />
       <Header />
       <Highlights />
