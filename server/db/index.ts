5eca6074a270f319490e1780580414cc18f0818f
OGEmanuel <emanuolumide@gmail.com>
2024-10-24 17:44:42 +0100

    fix:attempt at build fix

--- server/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
-
-export const db = drizzle(sql);
-
-// lib/db.ts
-
-// Create drizzle database instance
-// export const db = drizzle(pool, { schema });
